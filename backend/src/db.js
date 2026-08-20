9c6ae6d271811b82ecdf3e477fa7ef0917021a05
phatarapol777-jpg <phatarapol777@gmail.com>
2026-08-20 21:57:08 +0700

    fix: create data directory before opening SQLite database

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const fs = require('fs');
 const Database = require('better-sqlite3');
 
-const db = new Database(path.join(__dirname, '..', 'data', 'app.db'));
+const dataDir = path.join(__dirname, '..', 'data');
+fs.mkdirSync(dataDir, { recursive: true });
+const db = new Database(path.join(dataDir, 'app.db'));
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
 
